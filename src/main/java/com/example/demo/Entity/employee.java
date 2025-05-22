5d51d9b4b24146d64f6d4095573c6be31a2b8d74
Sambhajibhadre <sambhajibhadre777@gmail.com>
2025-05-22 15:44:16 +0530

    employees

---
+++ src/main/java/com/example/demo/Entity/employee.java
@@ -0,0 +1,51 @@
+package com.example.demo.Entity;
+import jakarta.persistence.*;
+
+public class employee {
+	
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+
+	    private String name;
+	    private String email;
+	    private String department;
+		public employee() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+		public employee(Long id, String name, String email, String department) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.email = email;
+			this.department = department;
+		}
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getDepartment() {
+			return department;
+		}
+		public void setDepartment(String department) {
+			this.department = department;
+		}
+	    
+	    
+
+}
